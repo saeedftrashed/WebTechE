10cdb524b0a8663578f63bfea46d327406035429
saeedftrashed <72831083+saeedftrashed@users.noreply.github.com>
2020-12-03 16:34:48 -0500

    Add files via upload

---
+++ Lab Task 6/config.php
@@ -0,0 +1,7 @@
+<?php
+#Database Details
+define("DB_HOST","127.0.0.1");
+define("DB_NAME","labtask6_aiub");
+define("DB_TABLE","login");
+define("DB_USER","eppti");
+define("DB_PASSWORD","password");
